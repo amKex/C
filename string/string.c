6f58b375576354beb15185138b6f386732b1bf12
Amit <amitbiswas53467@gmail.com>
2023-12-26 18:25:30 +0530

    added a new folder for all string question and ans

---
+++ string/string.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void printStr(char arr[]);
+
+int main(){
+    char firstName[] = "Amit";
+    char lastName[] = "Biswas";
+    printf("printed this name using for loop > ");
+    printStr(firstName);
+    printStr(lastName);
+
+    //taking inputs and then printing them using "%s"   
+    char fullName[50];
+    printf("\n");
+    scanf("%s", fullName);
+    printf("printed name using string format specifier > %s", fullName);
+
+    return 0;
+}
+//printing strings using a function 
+void printStr(char arr[]){
+    for (int i=0; arr[i] != '\0'; i++){
+        printf("%c", arr[i]);
+    }
+    printf(" ");
+}
